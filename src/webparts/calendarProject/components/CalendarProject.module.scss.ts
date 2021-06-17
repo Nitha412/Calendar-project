ec84f76414f10a6e25c121a168fb526837a073b7
Nitha412 <84444954+Nitha412@users.noreply.github.com>
2021-06-17 12:21:11 +0530

    Add files via upload

---
+++ src/webparts/calendarProject/components/CalendarProject.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./CalendarProject.module.css");
+const styles = {
+  calendarProject: 'calendarProject_deec1c94',
+  container: 'container_deec1c94',
+  row: 'row_deec1c94',
+  column: 'column_deec1c94',
+  'ms-Grid': 'ms-Grid_deec1c94',
+  title: 'title_deec1c94',
+  subTitle: 'subTitle_deec1c94',
+  description: 'description_deec1c94',
+  button: 'button_deec1c94',
+  label: 'label_deec1c94'
+};
+
+export default styles;
+/* tslint:enable */
---
+++ src/webparts/calendarProject/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICalendarProjectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CalendarProjectWebPartStrings' {
+  const strings: ICalendarProjectWebPartStrings;
+  export = strings;
+}
